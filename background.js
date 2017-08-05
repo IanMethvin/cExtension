7d1dfbfa11b6d6bbdd7e5e25a3352f86205d238f
== <imethvin@uci.com>
2017-08-04 18:47:47 -0700

    Removed vulgar comments and code cleanup

--- background.js
+++ background.js
@@ -1,16 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  // chrome.tabs.executeScript({
-  //   code: 'document.body.style.backgroundColor="red"'
-  // });
-  
-  	 chrome.tabs.executeScript(null, {
-		 file: 'slider.js'
-		//code: "addSlider()";
-	 });
-	 
-
+	chrome.tabs.executeScript(null, {
+	 	//code: ""
+		file: 'slider.js'
+	});
 });
 
 
--- slider.js
+++ slider.js
@@ -1,25 +1,7 @@
-// $(document).ready(function(){
-// 	$("#checkPage").click(function() {	
-// 		alert("Fuck bud I did something");
-// 	});
-// });
-
-
 (function() {
 
 debugger;
-	//document.body.style.backgroundColor="red"
 
-	// var parent = document.getElementsByTagName("body")[0];
-	// var child = document.createElement("div");
-	// child.setAttribute("id", "slider");
-	// //child.style.cssText = 'width:100%;height:24px;background-color:black;color:white;font-size:1.3em;padding-top:2px;clear:both;position:fixed;top:0;z-index:2147483647;';
-	// child.innerHTML = '<marquee>heeeeeeeeeeeeellllllllloooooooooooooooooo</marquee>';//<div id="fb-root" style="width:100%;background-color:red">YABOOBAY</div>'
-	
-	// parent.children[0].style.marginTop = "24px";
-	// // parent.appendChild(child);
-	// parent.insertBefore(child, parent.children[0]);
-
-	//$("body").children[0].css("marginTop", "24px");
+	$("body").children()[0].style.marginTop = "24px"
 	$("body").prepend('<div id="slider"><marquee>heeeeeeeeeeeeellllllllloooooooooooooooooo</marquee></div>');
 })();
